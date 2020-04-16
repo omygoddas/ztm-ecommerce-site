18648ded63384ac2da02b98ccb0ac7d75ed337e4
ChihCheng Lai <omygoddas@gmail.com>
2020-04-16 20:27:05 +0800

    Added checkout page, checkout item component and routing function

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -23,3 +23,11 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedCost, cartItem) =>
+      accumulatedCost + cartItem.quantity * cartItem.price,
+    0
+  )
+);
