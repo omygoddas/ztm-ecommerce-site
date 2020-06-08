86daa6ce23a8e58e2f9a03faa8f9d86033afc3ff
ChihCheng Lai <omygoddas@gmail.com>
2020-06-08 21:58:53 +0800

    Converted collection with styled-components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,19 +6,23 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectShopCollection } from '../../redux/shop/shop.selectors';
 
-import './collection.styles.scss';
+import {
+  CollectionPageContainer,
+  TitleContainer,
+  ItemContainer,
+} from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <TitleContainer>{title}</TitleContainer>
+      <ItemContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemContainer>
+    </CollectionPageContainer>
   );
 };
 
