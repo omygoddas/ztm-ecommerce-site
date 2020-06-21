443e36bc45b5c279e5196a0e69182c2ef935711c
ChihCheng Lai <omygoddas@gmail.com>
2020-06-21 22:36:29 +0800

    Add redux-thunk to handle async actions

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -16,3 +16,8 @@ export const selectShopCollection = (collectionUrlParam) =>
   createSelector([selectShopCollections], (collections) =>
     collections ? collections[collectionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
