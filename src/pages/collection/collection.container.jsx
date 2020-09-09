a98e88283d81bf83cdfb65abde35a6831439dbd9
ChihCheng Lai <omygoddas@gmail.com>
2020-09-09 21:08:29 +0800

    Made graphql query with title variable on shop page

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Spinner from '../../components/spinner/spinner.component';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+import CollectionPage from './collection.component';
+
+const GET_COLLECTION_BY_TITLE = gql`
+  query getCollectionsByTitle($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+const CollectionsPageContainer = ({ match }) => (
+  <Query
+    query={GET_COLLECTION_BY_TITLE}
+    variables={{ title: match.params.collectionId }}
+  >
+    {({ loading, data }) => {
+      if (loading) return <Spinner />;
+      const { getCollectionsByTitle } = data;
+      return <CollectionPage collection={getCollectionsByTitle} />;
+    }}
+  </Query>
+);
+
+export default CollectionsPageContainer;
