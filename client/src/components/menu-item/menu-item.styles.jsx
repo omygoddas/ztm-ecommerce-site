8adecb948a29cfc1f5fd33081d45dc9ebae41f5f
ChihCheng Lai <omygoddas@gmail.com>
2020-10-18 17:47:04 +0800

    Update styles to use media queries for mobile

--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -35,6 +35,10 @@ export const MenuItemContainer = styled.div`
   &:last-child {
     margin-right: 7.5px;
   }
+
+  @media screen and (max-width: 800px) {
+    height: 200px;
+  }
 `;
 
 export const BackgroundImage = styled.div`
