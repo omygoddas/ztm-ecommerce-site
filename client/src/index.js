07086c63e6757f906566487976c24430b832f285
ChihCheng Lai <omygoddas@gmail.com>
2020-11-16 18:08:54 +0800

    Update to be PWA compliant

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
+
+import * as serviceWorker from './serviceWorker';
 
 import { store, persistor } from './redux/store';
 
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -17,6 +18,7 @@ app.use(compression());
 // Process all body from coming requests and convert it to json format
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // cors: cross-origin resource sharing
 // allow to pass requests from our frontend to our backend server
@@ -38,6 +40,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log(`Server running on port ${port}...`);
+});
+
+app.get('./service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
